6341c36c553fc71d7d8abe6504a3a077adeab7d0
s92025592025 <s92025592025@users.noreply.github.com>
2016-03-24 14:40:16 -0700

    Passed
    Passed Uva by 0.165
    
    use long to avoid stack overflow

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ class Main{
 			if(!verify(base1, number.toUpperCase())){
 				System.out.println(number + " is an illegal base " + base1 + " number");
 			}else{
-				System.out.println(number + " base " + base1 + " = " + convert(base1, number, base2) + " base " + base2);
+				System.out.println(number + " base " + base1 + " = " + convert(base1, number.toUpperCase(), base2) + " base " + base2);
 			}
 		}
 	}
@@ -19,7 +19,7 @@ class Main{
 	public static boolean verify(int base, String number){
 		for(int i = 0; i < number.length(); i++){
 			if(number.charAt(i) > 64 && number.charAt(i) < 91){
-				if(number.charAt(i) - 65 >= base){
+				if(number.charAt(i) - 55 >= base){
 					return false;
 				}
 			}else{
@@ -33,20 +33,26 @@ class Main{
 	}
 
 	public static String convert(int base1, String number, int base2){
-		int sum = 0;
-		int decimal = 1;
+		long sum = 0;
+		long decimal = 1;
 		for(int i = number.length() - 1; i > -1; i--){
 			if(number.charAt(i) > 64 && number.charAt(i) < 91){
-				sum += (number.charAt(i) - 65) * decimal;
+				sum += (number.charAt(i) - 55) * decimal;
 			}else{
 				sum += (number.charAt(i) - 48) * decimal;
 			}
 
 			decimal *= base1;
 		}
+
 		String converted = "";
+		if(sum == 0){converted = "0";}
 		while(sum != 0){
-			converted = sum % base2 + converted;
+			if(sum % base2 < 10){
+				converted = sum % base2 + converted;
+			}else{
+				converted = (char)(sum % base2 + 55) + converted;
+			}
 			sum = sum / base2;
 		}
 
